ef6943bd71ec3fca3df022e03c14a7a2dc282ca1
Praveen Singh <psraj86@gmail.com>
2020-11-09 06:25:21 +0530

    added page not found error page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { StudentDetailsComponent } from './student-details/student-details.component';
 import { StudentComponent } from './student/student.component';
 
 const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'student',
+    pathMatch: 'full'
+  },
   {
     path: 'student',
     component: StudentComponent,
@@ -12,6 +18,10 @@ const routes: Routes = [
     path: 'studentDetails',
     component: StudentDetailsComponent,
   },
+  {
+    path:"**",
+    component:PageNotFoundComponent
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StudentComponent } from './student/student.component';
 import { StudentDetailsComponent } from './student-details/student-details.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StudentComponent,
-    StudentDetailsComponent
+    StudentDetailsComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
